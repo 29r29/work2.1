df0414336a96315e7083c17b3dfaa7b582b71432
bezzybr <bezyybr29@gmail.com>
2024-05-19 21:23:34 +1000

    project initialized

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class='card'>
+        <img alt='аватар' src='https://p.turbosquid.com/ts-thumb/Fi/IaGxZV/hdjm94IA/blue1/jpg/1368542068/600x600/fit_q87/0e7a09dd6690b237f62a93189cd211b147238356/blue1.jpg' class='img'/>
+        <div class='inf'>Rost</div>
+        <div class='inf'>Дата рождения: 29.09.2009</div>
+        <div class='inf'>Участник с 08.08.2021</div>
+        <div class='inf'>Связь: rost.khv@yandex.ru / +7 996 388-11-48</div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
+describe('tests #5' , () =>{
+test('image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const image = screen.getByRole('img')
+  expect(image).toBeInTheDocument()
 });
+
+test('nickname', () => {
+  render(<App />);
+  const nickname = screen.getByText('Rost')
+  expect(nickname).toBeInTheDocument()
+});
+
+test('dates', async () => {
+  render(<App />);
+  const regDate = await screen.findByText('Участник с 08.08.2021')
+  expect(regDate).toBeInTheDocument()
+
+  const birthDate = await screen.findByText('Дата рождения: 29.09.2009')
+  expect(birthDate).toBeInTheDocument()
+});
+
+test('information all', () => {
+  render(<App />);
+  const alls = screen.getByText('Связь: rost.khv@yandex.ru / +7 996 388-11-48')
+  expect(alls).toBeInTheDocument()
+});
+}
+)
